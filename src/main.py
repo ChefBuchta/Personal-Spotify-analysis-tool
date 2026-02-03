80e5aea606b4bccd1eb442f0f8f2a24afa5e6c97
Martin Horak <martin.horak.459@gmail.com>
2026-02-03 11:13:10 +0100

    main: Added functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import glob 
 
+# INPUT 
 path = 'data/Streaming_History_Audio*.json'
 files = glob.glob(path)
 dfList = [pd.read_json(f) for f in files]
@@ -11,19 +12,42 @@ df['ts'] = pd.to_datetime(df['ts'])
 
 
 df["ms_played"] = df['ms_played'] / 1000
-df.rename(columns={'ms_played': 'sec_played'}, inplace=True)
+df.rename(columns={'ms_played': 'sec_played', 'master_metadata_album_artist_name': 'artist_name','master_metadata_track_name': "track_name" }, inplace=True)
 # print(df.head())
-# TIME SPENT LISTENING SUM
 
-timeSpent = df["sec_played"].sum()
-# print(f"TIME SPENT LISTENING {(timeSpent/3600).round(2)} HOURS {((timeSpent/60)%60).round(2)} MINUTES {(timeSpent%60).round(2)} SECONDS")
 
-print("\nTOP 10 ARTISTS BY SONGS PLAYED")
-print(df['master_metadata_album_artist_name'].value_counts().head(10))
+# PRINTS TIME SPENT LISTENING SUM
+def printTimeSpentListening(df: pd.DataFrame) -> None:
+    timeSpent = df["sec_played"].sum()
+    print(f"TIME SPENT LISTENING {(timeSpent/3600).round(2)} HOURS {((timeSpent/60)%60).round(2)} MINUTES {(timeSpent%60).round(2)} SECONDS")
 
-print("\nTOP 10 ARTISTS BY PLAYTIME")
-print((df.groupby('master_metadata_album_artist_name')['sec_played'].sum().sort_values(ascending=False).head(10) / 3600).round(2))
+# RETURNS THE TOP 10 ARTIST BY NUMBER OF PLAYS 
+def getTop10ArtistBySongs(df: pd.DataFrame) -> pd.DatFrame:
+   return df['artist_name'].value_counts().head(10)  
 
-print("\nTOP 10 SONGS BY TIMES PLAYED")
-print(df[['master_metadata_track_name', 'master_metadata_album_artist_name']].value_counts().head(10))
+# PRINTS THE TOP 10 ARTIST BY NUMBER OF PLAYS
+def printTop10ArtistBySongs(df):
+    artists = getTop10ArtistBySongs(df)
+    print("\nTOP 10 ARTISTS BY SONGS PLAYED")
+    print(artists)
+
+# RETURNS THE TOP 10 SONGS BY PLAYTIME 
+def getTop10ArtistByPlay(df: pd.DataFrame) -> pd.DataFrame:
+    return (df.groupby('artist_name')['sec_played'].sum().sort_values(ascending=False).head(10) / 3600).round(2)
+
+# PRINTS THE TOP 10 SONGS BY PLAYTIME 
+def printTop10ArtistBySongs(df):
+    artists = getTop10ArtistByPlay(df)
+    print("\nTOP 10 ARTISTS BY PLAYTIME")
+    print(artists)
+
+# RETURNS THE TOP 10 SONGS BY PLAYTIME 
+def getTop10SongsByPlay(df: pd.DataFrame) -> pd.DataFrame:
+    return df[['track_name', 'artist_name']].value_counts().head(10) 
+
+# PRINTS THE TOP 10 SONGS BY PLAYTIME 
+def printTop10SongsByPlay(df: pd.DataFrame) -> pd.DataFrame:
+    songs = getTop10SongsByPlay(df)
+    print("\nTOP 10 SONGS BY TIMES PLAYED")
+    print(songs)
 
